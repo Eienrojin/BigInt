c0a759dbab0529b78e7b98f1a5b074070d33ab51
user <user@C1014>
2022-04-20 15:24:15 +0300

    Fix add

--- BigInt/Program.cs
+++ BigInt/Program.cs
@@ -6,17 +6,34 @@ namespace BigInt
     {
         static void Main()
         {
-            bigInt a = new bigInt("324", 1);
-            bigInt b = new bigInt("32", 1);
+            Test();
+        }
 
+        static void Test()
+        {
+            BigInteger num1 = 151;
+            BigInteger num2 = -38;
+            BigInteger resultI = num2 + num1;
+
+            bigInt a = new bigInt(num1.ToString());
+            bigInt b = new bigInt(num2.ToString());
+            bigInt c = new bigInt("0");
+
+            Console.WriteLine("До действий");
             Console.WriteLine(a);
             Console.WriteLine(b);
-            Console.WriteLine(a > b);
 
-            if (a <= b)
-            {
-                Console.WriteLine("Выполнилось");
-            }
+            //Действие тут
+            c = a + b;
+
+            Console.WriteLine("\nПосле сложения");
+            Console.WriteLine($"{a}" +
+                            $"\n{b}" +
+                            $"\n-------" +
+                            $"\n{c}");
+
+            Console.WriteLine("\nЭталон");
+            Console.WriteLine(resultI);
         }
     }
 }
--- BigInt/bigInt.cs
+++ BigInt/bigInt.cs
@@ -1,17 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*TODO*/
+/*
+* 
+* СЛОЖЕНИЕ ПОЛОЖИТЕЛЬНОГО И ОТРИЦАТЕЛЬНОГО ЧИСЛА, ГДЕ |-А| > B
+* ДОБАВИТЬ ПРОВЕРКУ ЧИСЛА НА ВВОД
+* ОТНИМАНИЕ ЧИСЛА В УМЕ
+*/
+
+/*СДЕЛАНО*/
+/*
+ * ПОФИКСИТЬ СЛОЖЕНИЕ ТИПА 9000+1000 = 0000 
+ *
+ * ПАРСИНГ ЗНАКА ЧИСЛА ПО ПЕРВОМУ ЗНАКУ
+ * 0     - НУЛЕВОЕ ЗНАЧЕНИЕ
+ * -432  - ОТРИЦАТЕЛЬНОЕ
+ * 432   - ПОЛОЖИТЕЛЬНОЕ
+ * 
+ * ИММУТАЦИЯ МЕТОДА ADD
+ */
 
 namespace BigInt
 {
-    internal struct bigInt : IComparable<bigInt>
+    internal class bigInt : IComparable<bigInt>
     {
-        public bigInt(string values, int positive)
+        public bigInt(string values)
         {
             Values = new();
-            Positive = positive;
+
+            if (values[0] == '0') Positive = 0;
+            if (values[0] != '0' && values[0] != '-') Positive = 1;
+
+            if (values[0] == '-')
+            {
+                Positive = -1;
+                values = values.Remove(0, 1);
+            }
 
             for (int i = 0; i < values.Length; i++)
             {
@@ -21,51 +43,170 @@ namespace BigInt
             }
         }
 
-        private List<sbyte> Values { get; set; }
-
-        private int Positive { get; set; }
-
-        /*------CALCULATING------*/
-        public static bigInt Add(bigInt left, bigInt right)
+        public bigInt(bigInt other)
         {
-
+            Values = other.Values;
+            Positive = other.Positive;
         }
 
-        /*------COMPARISON------*/
+        private List<sbyte> Values { get; set; }
 
+        private int Positive { get; set; } = 0;
+
+        /*------------CALCULATING------------*/
+
+        /*------------ADD------------*/
+        public static bigInt operator +(bigInt left, bigInt right)
+        {
+            return Add(left, right);
+        }
+
+        //Note: it work only with positive values
+        private static bigInt Add(bigInt a, bigInt b)
+        {
+            // Number in mind. It needs for values bigger than 9
+            int x, y, tempResult, result;
+
+            int overflow = 0;
+            string tempString;
+            string tempString2;
+            bigInt bigResult = new("0");
+
+            List<sbyte> tempList = new List<sbyte>();
+
+            bigInt left = new bigInt(tempString);
+            bigInt right = new bigInt(tempString2);
+
+            MakeEqualLength(left, right);
+
+            for (int i = left.Values.Count - 1; i >= 0; i--)
+            {
+                x = left.Values[i];
+                y = right.Values[i];
+
+                if (left.Values[i] > right.Values[i])
+                    tempResult = (x * left.Positive) + (y * right.Positive) + overflow;
+                else
+                    tempResult = (y * right.Positive) + (x * left.Positive) + overflow;
+
+                overflow = tempResult / 10;
+                result = tempResult % 10;
+
+                tempList.Add((sbyte)result);
+
+                if (i == 0 && Math.Abs(overflow) == 1)
+                {
+                    tempList.Insert(tempList.Count, 1);
+                }
+            }
+
+            GetAbsList(tempList);
+            tempList.ToString();
+
+            for (int i = 0; i <= tempList.Count - 1; i++)
+            {
+                bigResult.Values.Add(tempList[i]);
+            }
+
+            //preparing stage
+            //delete zeroes from the front and reverse
+            bigResult.Values.RemoveAt(0);
+            bigResult.Values.Reverse();
+
+          /*  DeleteUnusualZeroes(bigResult);*/
+
+            bigResult.Positive = SetSignForAdd(left, right);
+
+            return bigResult;
+        }
+
+        private static int SetSignForAdd(bigInt left, bigInt right)
+        {
+            if (left.Positive == right.Positive)
+            {
+                return left.Positive;
+            }
+
+            return 0;
+
+/*            if (left.Positive == -1 || right.Positive == -1)
+            {
+                for (int i = 0; i < ; i++)
+                {
+
+                }
+            }*/
+        }
+
+        private static void GetAbsList(List<sbyte> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] = Math.Abs(list[i]);
+            }
+        }
+
+        // Adds zeroes to the front of some List<>, if count of elements not equal
+        private static void MakeEqualLength(bigInt left, bigInt right)
+        {
+            if (left.Values.Count != right.Values.Count)
+            {
+                if (left.Values.Count > right.Values.Count)
+                    for (int i = right.Values.Count; i < left.Values.Count; i++)
+                        right.Values.Insert(0, 0);
+
+                if (left.Values.Count < right.Values.Count)
+                    for (int i = left.Values.Count; i < right.Values.Count; i++)
+                        left.Values.Insert(0, 0);
+            }
+        }
+
+        private static void DeleteUnusualZeroes(bigInt bigInt)
+        {
+            for (int i = 0; i < bigInt.Values.Count; i++)
+            {
+                if (bigInt.Values[i] == 0)
+                    bigInt.Values.RemoveAt(i);
+                else
+                    break;
+            }
+        }
+
+        /*------------COMPARISON------------*/
         public static bool operator >(bigInt left, bigInt right)
         {
             return left.CompareTo(right) == 1;
         }
-
         public static bool operator <(bigInt left, bigInt right)
         {
             return left.CompareTo(right) == -1;
         }
-
+        public static bool operator >=(bigInt left, bigInt right)
+        {
+            return left > right || left == right;
+        }
+        public static bool operator <=(bigInt left, bigInt right)
+        {
+            return left < right || left == right;
+        }
         public static bool operator ==(bigInt left, bigInt right)
         {
             return left.CompareTo(right) == 0;
         }
-
         public static bool operator !=(bigInt left, bigInt right)
         {
             return left.CompareTo(right) != 0;
         }
 
-        public static bool operator >=(bigInt left, bigInt right)
-        {
-            return left > right || left == right;
-        }
-
-        public static bool operator <=(bigInt left, bigInt right)
-        {
-            return left < right || left == right;
-        }
-
         public override string ToString()
         {
-            string result = string.Join("", Values);
+            string result;
+
+            if (Positive == -1)
+                result = "-" + string.Join("", Values);
+            else
+                result = string.Join("", Values);
+
 
             return result;
         }
@@ -74,9 +215,7 @@ namespace BigInt
         {
             if (Positive != other.Positive) return Positive.CompareTo(other.Positive);
 
-
             if (Values.Count != other.Values.Count) return Values.Count.CompareTo(other.Values.Count) * Positive;
-
 
             if (Values.Count != other.Values.Count) return Values.Count.CompareTo(other.Values.Count);
 
@@ -87,6 +226,6 @@ namespace BigInt
             }
 
             return 0;
-        }
+        }        
     }
 }
